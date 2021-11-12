6757a5fc408531c67dffa9eb1e6b1706f3d0431b
paperboygold <89246410+paperboygold@users.noreply.github.com>
2021-11-12 14:29:03 +1100

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 '''
-KIT103 Project: Enigma Machine Plugboard
-
-Name: Lucas Townsend
-ID: 597588
+Enigma Machine Plugboard
 
 This file is for generating new rotors and reflector objects.'''
 
